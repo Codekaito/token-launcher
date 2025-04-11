30f2640a45e1a73e553285d954aa2cfe6e3630fd
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2025-04-11 19:25:34 +0200

    commit

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-// @natspec ERC20 Token Factory
-// @author @0xCodeKaito
-// @dev This contract is used to create new ERC20 tokens.
+/**
+ * @title ERC20 Token Factory
+ * @author @0xCodeKaito
+ * @notice Contract for creating new ERC20 tokens
+ * @dev This contract allows users to deploy new ERC20 token contracts
+ */
+
 contract TokenFactory {
 
 }
